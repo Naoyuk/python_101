fdde0bbcaf2d3fe0a188f5554b25bf2707c61062
Naoyuki Ishida <39.ishida@gmail.com>
2021-02-26 14:39:01 -0800

    changed places of each source files and test files

--- test/test_hello.py
+++ test/test_hello.py
@@ -1,5 +1,10 @@
+import os
+import sys
 # unittestモジュールを読み込んで利用
 import unittest
+
+# importで参照するパスをappend
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 
 class TestFunc(unittest.TestCase):
     def test_func(self):
